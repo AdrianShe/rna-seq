f8c64234788c2afa13d39b888b7875db9225f4b5
bhcoop4 <bhcoop4@mordor.(none)>
2015-08-28 17:45:40 -0400

    Update StringTie parser

--- output_parser/parseStringTie.R
+++ output_parser/parseStringTie.R
@@ -1,57 +1,54 @@
 ## Parse output for stringtie
 library(data.table)
+library(pbapply)
 
-parseStringtie <- function(file) {
-    lines <- readLines(file)
-    ## Get lines with an FPKM value
-    lines <- lines[grep("FPKM", lines)]
-    annotatedLines <- lines[grep("reference_id", lines)]
-    parsedLines <- pblapply(annotatedLines, parseAnnotatedLine)
-    finalFrame <- rbindlist(parsedLines)
-    return(finalFrame)
+parseStringtieFile <- function(file) {
+  lines <- read.delim(file, header = FALSE, skip = 2, colClasses=c(rep("character", 3), rep("integer", 3), rep("character", 3)), nrows=1200000)
+  ## Get lines with an FPKM value and a reference annotation
+  info <- as.character(lines[,9])
+  lines <- lines[grep("FPKM", info),]
+  annotatedLines <- lines[grep("reference_id", as.character(lines[,9])),]
+
+  ## Get tokens from ; info
+  tokens <- strsplit(as.character(annotatedLines[, 9]), ";")
+  transcript_id <- sapply(tokens, function (x) gsub(" reference_id ", "", x[3]))
+  FPKM <- sapply(tokens, function (x) as.numeric(gsub(" FPKM ", "", x[7])))
+  names(FPKM) <- transcript_id
+  return(FPKM)
 }
 
-
-
-
-parseAnnotatedLine <- function(line) {
-  tokens <- unlist(strsplit(line, ";"))
-  tokens <- strsplit(tokens, "\"")
-  getItem <- function(attr) {
-    tokens[[grep(attr, tokens)]][2]
-  }
-
-  frame <- data.frame(
-    "reference_id" = getItem("reference_id"),
-    "ref_gene_id" = getItem("ref_gene_id"),
-    "ref_gene_name" = getItem("ref_gene_name"),
-    "fpkm" = as.numeric(getItem("FPKM")),
-    stringsAsFactor = FALSE
-    )
-  
-  return(frame)
+isComplete <- function(x) {
+  length(readLines(x, 3)) > 2
 }
 
-parseStringtieNoGene <- function(file) {
-  lines <- readLines(file)
-  ## Get lines with an FPKM value
-  lines <- lines[grep("FPKM", lines)]
-  annotatedLines <- lines[grep("reference_id", lines)]
-  parsedLines <- unlist(pblapply(annotatedLines, parseAnnotatedLineNoGene))
-  return(parsedLines)
-}
+##
+args <- commandArgs()
+print(args)
+path <- args[1] ## hack to make this work properly
+align <- args[2]
+files <- list.files(path, full.names  = TRUE)
+files_ccle <- sapply(files, function (x) paste0(x, "/CCLE/stringtie_output.gtf"))
+#files_gne <- sapply(files, function (x) paste0(x, "/GNE/stringtie_output.gtf"))
 
-parseAnnotatedLineNoGene <- function(line) {
-  tokens <- unlist(strsplit(line, ";"))
-  tokens <- strsplit(tokens, "\"")
-  getItem <- function(attr) {
-    tokens[[grep(attr, tokens)]][2]
-  }
-  ref <- getItem("reference_id")
-  fpkm <- as.numeric(getItem("FPKM"))
-  names(fpkm) <- ref
-  return(fpkm)
-}
+files_ccle_comp <- Filter(file.exists, files_ccle)
+files_ccle_comp <- Filter(isComplete, files_ccle_comp)
+print(paste("Problematic dirs or no StringTie output: ", setdiff(files_ccle, files_ccle_comp)))
+print(paste(length(files_ccle_comp), "CCLE files to process"))
 
-#vals <- parseStringtie("Desktop/stringtie_output.gtf")
-#print(str(vals))
+#files_gne_comp <- Filter(file.exists, files_gne)
+#files_gne_comp <- Filter(isComplete, files_gne_comp)
+#print(paste("Problematic dirs or no StringTie output: ", setdiff(files_gne, files_gne_comp)))
+#print(paste(length(files_gne_comp), "GNE files to process"))
+
+message("Parsing CCLE output files")
+parsedLines_ccle  <- pblapply(files_ccle_comp, parseStringtieFile)
+names(parsedLines_ccle) <- files_ccle_comp
+
+#message("Parsing GNE output files")
+#parsedLines_gne  <- pblapply(files_gne_comp, parseStringtieFile)
+#names(parsedLines_gne) <- files_gne_comp
+
+save(parsedLines_ccle, file = paste0(align, "_ccle_stringtie_res.RData"))
+
+
+
